016e9a65ef263bc612c6b92e07e41f6ad9671cb2
kaiomudkt <kaiomudkt@gmail.com>
2020-08-19 14:32:54 -0400

    arquivos de inicialização do thema

---
+++ cpt-agendamento.php
@@ -0,0 +1,16 @@
+<?php
+/*
+	Este arquivo é responsável por juntar os dados do atendimento em sí,
+	como relacionar os usuários: paciente e atendente, e outros dados como data, tipo de atendimento, especialidade, duração do atendimento
+*/
+/*
+dados a ser armazenados:
+identificador paciente,
+identificador especialista,
+duração do atendimento,
+local do atendimento,
+notas do especialista sobre o paciente,
+solicitação de remedios,
+(acho legal colocar dados dinamicos) 
+*/
+?>
